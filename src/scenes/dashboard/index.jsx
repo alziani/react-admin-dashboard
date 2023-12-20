496b80033d05952854f573f651720611d31a12cd
alziani <99541666+alziani@users.noreply.github.com>
2023-12-20 23:04:44 +0100

    commit

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
-export const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+function Dashboard() {
+  return (
+    <Box m="70px">
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
+      </Box>
+    </Box>
+  );
+}
+
+export default Dashboard;
